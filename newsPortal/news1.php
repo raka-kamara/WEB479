e73fd28ed622ff4f8ef46fe01349e07d71ff0fd5
kling <60285236+raka-kamara@users.noreply.github.com>
2024-01-05 23:35:03 +0600

    Add files via upload

---
+++ newsPortal/news1.php
@@ -0,0 +1,287 @@
+<html>
+    <head>
+        <title></title>
+        <link rel="stylesheet" href="style1.css">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/
+        fontawesome-free@6.2.1/css/fontawesome.min.css">
+    
+    </head>
+        <div class ="background">
+        <?php include 'navbar.php' ?>
+    
+    <section id = "home">
+            <div class = "main">
+                <h1>Welcome to EWU <div style = "color:#6495ED"> CLUB NEWS PORTAL</div></h1>
+                
+            </div>
+            <a href="news.php">
+            <button class="btn">
+                Get Started
+            </button>
+        </a>
+        </section>
+
+        </div>
+     <!-- Home ends here -->
+        
+        <!-- About -->
+
+    <section id = "about">
+    <h1 class="headings" style="color:bisque">ABOUT</h1>
+
+
+        <section id = "history">
+            <h1 class="headings1" style="color:#6495ED">HISTORY</h1> 
+            <div id="pic">
+                <img src="img1.jpg" alt="">
+        
+                 <div class="box">   
+                <p><h3>History of EWU</h3>The idea of establishing a private university to provide quality education at an affordable cost in Bangladesh was first mooted by a group of prominent academics, 
+                    business leaders, professionals and education enthusiasts led by Dr. Mohammed Farashuddin. With this end in view, this group formed a non-profit, non-political,
+                     charitable organization called Progoti Foundation for Education and Development (PFED). East West University is its first major project. Members of the Board of 
+                     Trustees of the University are: Mr. Jalaluddin Ahmed, Mr. S.M. Nousher Ali, Mr. Farooque B. Chaudhury, Dr. Rafiqul Huda Chaudhury, Mr. Syed Manzur Elahi, 
+                     Dr. Mohammed Farashuddin, Mr. Mohammed Zahidul Haque R.Ph., Dr. Saidur Rahman Lasker, Dr. Muhammad A. Mannan, Professor M. Mosleh-Uddin (Ceased to be a
+                      member as of 26 April 2016), Mr. Shelley A. Mubdi, Mr. M.A. Mumin, Dr. Khalil Rahman, Mr. H.N. Ashequr Rahman and Mrs. Razia Samad. After being accorded 
+                      permission by the Government under the Private University Act (Act 34) of 1992, East West University was launched in 1996. Classes started in September 1996
+                       with 6 faculty members and 20 students in the campus located at 43 Mohakhali C/A, Dhaka-1212. Today there are over 434 faculty members and approximately 10500 
+                       students enrolled through a process of selection. </p>
+            </div>
+        </div>
+    </section>
+        
+
+        <section id = "notice">
+            <h1 class="headings1" style="color:#6495ED">VISION</h1> 
+            <div id="pic">
+                <img src="img1.jpg" alt="">
+                
+                 <div class="box">   
+                <p> <h3>Vision of EWU CLUBS</h3> East West University was established with interrelated objectives of augmenting national capacity fo
+                r tertiary education to a larger number of students, and thereby contributing to the country's human capital development process.
+                EWU is committed to equip its graduates with professional skills and innovative and critical thinking capable of taking advantage of 
+                emerging opportunities at home and abroad. By providing access to affordable university-level education to students from less-well-to-do 
+                sections of the society, EWU seeks to harness the power of individuals through higher education, by raising employable skills and improving 
+                access to job opportunities, thereby contributing to a more balanced and just society. The vision that inspired EWU is to be a topnotch learning 
+                center that combines excellence in education with strong human values and social responsibility, aiming to improve the quality of life, principally
+                 of its students. In the process, EWU strives to broaden human welfare by promoting a knowledge-based society and informed citizenry, with respect for,
+                  and commitment to the ideals of a pluralism and democracy. </p>
+            </div>
+        </div>
+    </section>
+
+        <section id = "faq">
+            <h1 class="headings1" style="color:#6495ED">FAQ</h1> 
+            <div class="row">
+                    
+                 <div class="box2">   
+                <p><h2> How many Clubs are there?</h2>EWU provides more than 20 clubs.
+                    All the clubs help students to show their hidden talents</p>
+                    </div>
+                     <div class="box2">   
+                        <p><h2> How to join in our Clubs?</h2>Join our Clubs through club reqruitment procedure that takes place
+                         in the beginning of every semester</p>
+                        </div>
+                       <div class="box2">   
+                           <p><h2> Who can Join in our Clubs?</h2>
+                           Only students of ewu can join.  
+                        </p>
+            </div>
+        </div>
+        </section>
+        
+        
+        </section>
+<!-- About ends here -->
+
+
+<!-- Career -->
+
+<section id = "career">
+    <h1 class="headings" style="color:bisque">TRENDY NEWS</h1>
+   
+    <section id = "moderator">
+        <h1 class="headings1" style="color:#6495ED">SCIENCE</h1> 
+        <div class="row">
+                
+        <div class="box3">   
+            <p><h2>INDEPENDENT NEWS</h2>
+            <a href="https://m.theindependentbd.com/online/science-tech" target="blank">
+            <button class="btn1">
+               Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+                <div class="box3">   
+            <p><h2> The Daily Star</h2>
+            <a href="https://www.thedailystar.net/tech-startup" target="blank">
+            <button class="btn1">
+                Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+
+                <div class="box3">   
+            <p><h2> AL JAZEERA</h2>
+            <a href="https://www.aljazeera.com/tag/science-and-technology/" target="blank">
+            <button class="btn1">
+                 Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+    
+    </div>
+    </section>
+
+
+    <section id = "president">
+        <h1 class="headings1" style="color:#6495ED">BUSINESS</h1> 
+        <div class="row">
+                
+             <div class="box3">   
+            <p><h2> INDEPENDENT NEWS</h2>
+            <a href="https://m.theindependentbd.com/online/business"  target="blank">
+            <button class="btn1">
+                 Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+                <div class="box3">   
+            <p><h2> The Daily Star</h2>
+            <a href="https://www.thedailystar.net/business"  target="blank">
+            <button class="btn1">
+                Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+
+                <div class="box3">   
+            <p><h2> AL JAZEERA</h2>
+            <a href="https://www.aljazeera.com/economy/"  target="blank">
+            <button class="btn1">
+                  Click Here
+            </button>
+           </a>
+
+                 </p>
+                </div>
+            </div>
+    </section>
+
+
+    <section id = "vice-president">
+        <h1 class="headings1" style="color:#6495ED">BANGLADESH NEWS</h1> 
+
+    <div class="container"><a href="https://www.prothomalo.com/" target="blank">
+        <div class="gallery">
+            <img src="l1.jpg" alt=""></a>
+            <div class="desc"> Click Image to Read News </div>
+        </div>
+  
+
+        <div class="gallery">
+            <a href="https://www.kalerkantho.com/" target ="blank">
+            <img src="l2.jpg" alt=""></a>
+            <div class="desc"> Click Image to Read News</div>
+        </div>
+
+        <div class="gallery">
+            <a href="https://samakal.com/" target ="blank">
+                <img src="l4.jpg" alt=""></a>
+            <div class="desc"> CClick Image to Read News </div>
+        </div>
+        
+        <div class="gallery">
+            <a href="https://www.jaijaidinbd.com/" target ="blank">
+            <img src="l3.jpg" alt=""></a>
+            <div class="desc"> Click Image to Read News </div>
+        </div>
+
+          
+        <div class="gallery">
+            <a href="https://bangla.bdnews24.com/" target ="blank">
+            <img src="l5.jpg" alt=""></a>
+            <div class="desc"> Click Image to Read News </div>
+        </div>
+
+        
+
+        <div class="gallery">
+            <a href="https://www.jagonews24.com/" target ="blank">
+            <img src="l6.jpg" alt=""></a>
+            <div class="desc"> Click Image to Read News</div>
+        </div>
+
+</section>
+
+
+</section>
+<!-- Career -->
+
+
+<!-- CLUBS -->
+
+<section id ="clubs">
+    <h1 class="headings" style="color:bisque">PHOTO GALLERY</h1>
+
+    <div class="container">
+        <div class="gallery">
+            <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s2.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </div>
+        </div>
+  
+
+        <div class="gallery">
+        <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s4.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </a></div>
+        </div>
+  
+        <div class="gallery">
+        <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s3.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </div>
+        </div>
+   
+        <div class="gallery">
+        <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s2.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </div>
+        </div>
+    
+        <div class="gallery">
+        <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s4.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </div>
+        </div>
+ 
+        <div class="gallery">
+        <a href="https://www.ewubd.edu/clubs" target="blank">
+            <img src="s2.jpg" alt=""></a>
+            <div class="desc"> Click to Visit Our Clubs </div>
+        </div>
+    </div>
+</section>
+
+<!-- CLUBS -->
+<!-- </section>
+
+<section id ="login">
+
+<?php include 'logout.php'; ?> 
+   
+</section> -->
+    </body>
+</html>
+
+<?php include'footer.php'; ?>
